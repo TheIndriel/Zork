290360b3bc2736d144bf3902aa33a7a323cff462
Justin Downs <justin.downs@knights.ucf.edu>
2021-09-27 00:08:39 -0400

    Zork 1.3

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -8,9 +8,49 @@ namespace Zork
         {
             Console.WriteLine("Welcome to Zork!");
 
-            string inputString = Console.ReadLine();
-            Commands command = ToCommand(inputString.Trim().ToUpper());
-            Console.WriteLine(command);
+            Commands command = Commands.UNKNOWN;
+            while (command != Commands.QUIT)
+            {
+                Console.Write("> ");
+                command = ToCommand(Console.ReadLine().Trim());
+
+                string outputString;
+                switch (command)
+                {
+                    case Commands.LOOK:
+                        Console.WriteLine("This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork' lies by the door.");
+                        break;
+
+                    case Commands.NORTH:
+                        Console.WriteLine("You moved NORTH.");
+                        break;
+
+                    case Commands.SOUTH:
+                        Console.WriteLine("You moved SOUTH.");
+                        break;
+
+                    case Commands.EAST:
+                        Console.WriteLine("You moved EAST.");
+                        break;
+
+                    case Commands.WEST:
+                        Console.WriteLine("You moved WEST.");
+                        break;
+
+                    case Commands.UNKNOWN:
+                        Console.WriteLine("Unknown command.");
+                        break;
+
+                    case Commands.QUIT:
+                        Console.WriteLine("Thank you for playing!");
+                        break;
+
+                    default:
+                        command = Commands.UNKNOWN;
+                        break;
+
+                }
+            }
         }
 
         private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
