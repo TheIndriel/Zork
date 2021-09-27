4b3170c393fe512f6778d6d1ee788413be0653fd
Justin Downs <justin.downs@knights.ucf.edu>
2021-09-27 00:17:43 -0400

    Zork 1.3 Revised

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -17,39 +17,27 @@ namespace Zork
                 string outputString;
                 switch (command)
                 {
+                    case Commands.QUIT:
+                        outputString = "Thank you for playing!";
+                        break;
+
                     case Commands.LOOK:
-                        Console.WriteLine("This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork' lies by the door.");
+                        outputString = "This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork' lies by the door.";
                         break;
 
                     case Commands.NORTH:
-                        Console.WriteLine("You moved NORTH.");
-                        break;
-
                     case Commands.SOUTH:
-                        Console.WriteLine("You moved SOUTH.");
-                        break;
-
                     case Commands.EAST:
-                        Console.WriteLine("You moved EAST.");
-                        break;
-
                     case Commands.WEST:
-                        Console.WriteLine("You moved WEST.");
-                        break;
-
-                    case Commands.UNKNOWN:
-                        Console.WriteLine("Unknown command.");
-                        break;
-
-                    case Commands.QUIT:
-                        Console.WriteLine("Thank you for playing!");
+                        outputString = $"You moved {command}.";
                         break;
 
                     default:
-                        command = Commands.UNKNOWN;
+                        outputString = "Unknown command.";
                         break;
-
                 }
+
+                Console.WriteLine(outputString);
             }
         }
 
